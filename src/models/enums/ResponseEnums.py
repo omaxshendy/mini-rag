f4c91858a94d89124ebb0b41734173c7d235feb8
omar essam shendy <abouomar.shendy@gmail.com>
2026-04-22 20:45:18 +0200

    Creating the data route and related files

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+class ResponseSignal(Enum):
+    FILE_VALIDATED_SUCCESS="FILE_VALIDATED_SUCCESSfully"
+    FILE_TYPE_NOT_SUPPORTED= "file_type_not_supported"
+    FILE_SIZE_EXCEEDED= "file_size_exceeded"
+    FILE_UPLOADED_SUCCESS="file_upload_success"
+    FILE_UPLOADED_FAILED="file_upload_failed"
+
+
