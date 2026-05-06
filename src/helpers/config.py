a6861a6244332113ac778923b827bbe17c7ae668
omar essam shendy <abouomar.shendy@gmail.com>
2026-05-06 13:54:43 +0300

    pgvector applied

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,4 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import List
+
 class Settings(BaseSettings):
     APP_NAME:str
     APP_VERSION:str
@@ -22,7 +24,8 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: str = None
     OPENAI_API_URL: str = None
     COHERE_API_KEY: str = None
-
+    
+    GENERATION_MODEL_ID_LITERAL: List[str] = None
     GENERATION_MODEL_ID: str = None
     EMBEDDING_MODEL_ID: str = None
     EMBEDDING_MODEL_SIZE: int = None
@@ -30,9 +33,11 @@ class Settings(BaseSettings):
     GENERATION_DAFAULT_MAX_TOKENS: int = None
     GENERATION_DAFAULT_TEMPERATURE: float = None
     
+    VECTOR_DB_BACKEND_LITERAL: List[str] = None
     VECTOR_DB_BACKEND : str
     VECTOR_DB_PATH : str
     VECTOR_DB_DISTANCE_METHOD: str = None
+    VECTOR_DB_PGVEC_INDEX_THRESHOLD: int = 100
     
     PRIMARY_LANG: str = "en"
     DEFAULT_LANG: str = "en"
--- src/routes/data.py
+++ src/routes/data.py
@@ -12,6 +12,7 @@ from models.ChunkModel import ChunkModel
 from models.AssetModel import AssetModel
 from models.db_schemas import DataChunk, Asset
 from models.enums.AssetTypeEnum import AssetTypeEnum
+from controllers import NLPController
 
 logger=logging.getLogger('uvicorn.error')
 
@@ -31,6 +32,14 @@ async def process_endpoint(request: Request,project_id:int,process_request:Proce
             db_client=request.app.db_client
     )
     project= await project_model.get_project_or_create_one(project_id=project_id)
+    
+    nlp_controller = NLPController(
+        vectordb_client=request.app.vectordb_client,
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+        template_parser=request.app.template_parser,
+    )
+    
     
     project_file_ids=[]
     asset_model= await AssetModel.create_instance(db_client=request.app.db_client)
@@ -77,6 +86,11 @@ async def process_endpoint(request: Request,project_id:int,process_request:Proce
         db_client=request.app.db_client
     )    
     if do_reset == 1:
+        # delete associated vectors collection
+        collection_name = nlp_controller.create_collection_name(project_id=project.project_id)
+        _ = await request.app.vectordb_client.delete_collection(collection_name=collection_name)
+
+        # delete associated chunks
         _ = await chunk_model.delete_chunks_by_project_id(
             project_id=project.project_id
         )
